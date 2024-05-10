b02356af15b8b643969f59bb742a4b10ffb4d91d
ChTole <ch.ariel.tole@gmail.com>
2024-05-09 21:19:38 -0300

    Agrego funcionalidad menú hamburguesa

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 import { cambiarSeccion } from "./ruteo.js";
+import { cambiarEstadoMenu } from "./menuBurger.js";
 
 // Seteo contenido por defecto al cargar el sitio
 window.addEventListener("DOMContentLoaded", evento =>cambiarSeccion("cursos"));
 
-// 1° Capturo todos los botones
+// Capturo todos los botones del nav
 const botonesNav = document.querySelectorAll('.menuSPA');
 
-// 2° Asigno la escucha del evento
+// Asigno la escucha del evento
 for (let boton of botonesNav){
     boton.addEventListener('click', evento => cambiarSeccion(boton.getAttribute('id')));
 }
+
+// Menú hamburguesa
+
+let menuBurger = document.querySelector('.menuBurger');
+let menuPpal = document.querySelector('#menuPpal');
+
+menuBurger.addEventListener('click', evento => cambiarEstadoMenu(menuPpal, menuBurger));
---
+++ js/menuBurger.js
@@ -0,0 +1,13 @@
+// La función cambia la visibilidad del menú a través de una clase (ver estilos) y el ícono de origen (hambuguesa o cierre para ocultar)
+
+function cambiarEstadoMenu(menu, icono) {
+    if (menu.classList[1] === 'visibleH'){
+        menu.className = 'visibleV';
+        icono.src = './icon/xmark-solid.svg';
+    } else {
+        menu.className = 'oculto visibleH';
+        icono.src = './icon/bars-solid.svg';
+    }
+}
+
+export {cambiarEstadoMenu};
