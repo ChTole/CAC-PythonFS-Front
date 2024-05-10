d29ba4eb79f5c74020c245bae661d0d2433b7ea8
ChTole <ch.ariel.tole@gmail.com>
2024-05-10 11:37:26 -0300

    Agrego navegación por hash y 404

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
-import { cambiarSeccion } from "./ruteo.js";
+import { mostrarHash } from "./ruteo.js";
 import { cambiarEstadoMenu } from "./menuBurger.js";
 
+// ********** SPA **********
 // Seteo contenido por defecto al cargar el sitio
-window.addEventListener("DOMContentLoaded", evento =>cambiarSeccion("cursos"));
+window.addEventListener("DOMContentLoaded", evento => mostrarHash());
+window.addEventListener("hashchange", evento => mostrarHash());
 
 // Capturo todos los botones del nav
-const botonesNav = document.querySelectorAll('.menuSPA');
+// const botonesNav = document.querySelectorAll('.menuSPA');
+// // Asigno la escucha del evento
+// for (let boton of botonesNav){
+//     boton.addEventListener('click', evento => cambiarSeccion(boton.getAttribute('id')));
+// }
 
-// Asigno la escucha del evento
-for (let boton of botonesNav){
-    boton.addEventListener('click', evento => cambiarSeccion(boton.getAttribute('id')));
-}
-
+// ********** Menú oculto para celu **********
 // Menú hamburguesa
-
 let menuBurger = document.querySelector('.menuBurger');
 let menuPpal = document.querySelector('#menuPpal');
 
--- js/ruteo.js
+++ js/ruteo.js
@@ -1,17 +1,42 @@
+// El inconveniente de utilizar SPA:
+// La URL es siempre la misma, entonces en el historial no impacta
+// ningún recorrido por el sitio, tampoco puedo compartir un lugar específico
+// del sitio.
+// Una posible solución (sin utilizar frameworks) es agregar un hash
+// al contenido dinámico. Para ésto también debo evaluar si la llamada a mi sitio
+// vienen acompañada de algún hash.
+
 // creo obj con las rutas
 const sitio = {
     cursos: './sitio/cursos.html',
     eventos: './sitio/eventos.html',
     acercaDe: './sitio/acercaDe.html',
-    registro: './sitio/registro.html'
+    registro: './sitio/registro.html',
+    404: './sitio/404.html'
 }
 
 // función que completa el contenido
 function cambiarSeccion(seccion){
     let seccionDinamica = document.querySelector('#contenidoSPA');
-    fetch(`${sitio[seccion]}`)
+    // fetch(`${sitio[seccion]}`)
+    fetch(`${seccion}`) // ahora recibe la ruta directamente
         .then(respuesta => respuesta.text())
         .then(datos => seccionDinamica.innerHTML = datos);
 }
 
-export {cambiarSeccion};
+function mostrarHash() {
+    let hashActual = window.location.hash;
+    if (hashActual.length == 0) {
+        hashActual = "#cursos";
+    }
+
+    let ruta = sitio[404]; // 404 por defecto
+
+    if (hashActual.substring(1,) in sitio) {
+        ruta = sitio[hashActual.substring(1,)];
+    }
+
+    cambiarSeccion(ruta);
+}
+
+export {mostrarHash, cambiarSeccion};
