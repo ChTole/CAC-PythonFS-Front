3f074af683318ff688ede6ac05c7b734602396b1
ChTole <ch.ariel.tole@gmail.com>
2024-05-22 18:09:26 -0300

    api/cursos - ver detalle

--- js/componentes/cursos.js
+++ js/componentes/cursos.js
@@ -1,13 +1,15 @@
 // Carga de los datos externos
 const cargarDatos = function() {
     let contenido = document.querySelector('.articulos');
-    fetch('./datos/cursos.json')
+    // fetch('./datos/cursos.json')
+    fetch('http://127.0.0.1:5000/cursos')
         .then(respuesta => respuesta.json())
-        .then(datos => cargarCursos(contenido, datos));
+        .then(datos => cargarCursos(contenido, datos.cursos));
 }
 
 // Modificación del HTML
 function cargarCursos(contenido, datos) {
+    console.log(datos)
     for (let d of datos) {
         let curso = document.createElement('article');
         let nombre = document.createElement('h3');
@@ -15,15 +17,15 @@ function cargarCursos(contenido, datos) {
         nombre.innerHTML = d.nombre;
         let imagenes = document.createElement('div');
         imagenes.className = 'contImg'
-        for (let i of d.imagen ) {
-            let logo = document.createElement("img");
-            logo.src = i.ubicacion;
-            logo.alt = i.textoAlt
-            logo.className = 'imgCurso';
-            imagenes.appendChild(logo);
-        }
+        // for (let i of d.imagen ) {
+        //     let logo = document.createElement("img");
+        //     logo.src = i.ubicacion;
+        //     logo.alt = i.textoAlt
+        //     logo.className = 'imgCurso';
+        //     imagenes.appendChild(logo);
+        // }
         let inicio = document.createElement('p');
-        inicio.innerHTML = `Fecha de inicio: ${new Date(d.inicio).toLocaleDateString()}`
+        inicio.innerHTML = `Fecha de inicio: ${new Date(d.fecha_inicio).toLocaleDateString()}`
         let masInfo = document.createElement('button');
         masInfo.className = "color4";
         masInfo.innerHTML= "Más información"
