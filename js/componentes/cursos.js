c08292c3c7cc49fa7c5cda2022e0a755e1f1e3a1
ChTole <ch.ariel.tole@gmail.com>
2024-06-20 17:59:10 -0300

    Modifico cursos - resta dividir cuenta/perfil

--- js/componentes/cursos.js
+++ js/componentes/cursos.js
@@ -1,35 +1,31 @@
 // Carga de los datos externos
 const cargarDatos = function() {
     let contenido = document.querySelector('.articulos');
-    // fetch('./datos/cursos.json')
-    fetch('http://127.0.0.1:5000/cursos')
+    fetch('http://127.0.0.1:5000/api-edtech/temas')
         .then(respuesta => respuesta.json())
-        .then(datos => cargarCursos(contenido, datos.cursos));
+        .then(datos => cargarCursos(contenido, datos));
 }
 
 // Modificación del HTML
 function cargarCursos(contenido, datos) {
-    console.log(datos)
     for (let d of datos) {
         let curso = document.createElement('article');
         let nombre = document.createElement('h3');
         nombre.className = "bold";
-        nombre.innerHTML = d.nombre;
+        nombre.innerHTML = d.tema;
         let imagenes = document.createElement('div');
         imagenes.className = 'contImg'
-        // for (let i of d.imagen ) {
-        //     let logo = document.createElement("img");
-        //     logo.src = i.ubicacion;
-        //     logo.alt = i.textoAlt
-        //     logo.className = 'imgCurso';
-        //     imagenes.appendChild(logo);
-        // }
-        let inicio = document.createElement('p');
-        inicio.innerHTML = `Fecha de inicio: ${new Date(d.fecha_inicio).toLocaleDateString()}`
+        for (let i of d.imagenes ) {
+            let logo = document.createElement("img");
+            logo.src = i.url_img;
+            logo.alt = i.texto_alt
+            logo.className = 'imgCurso';
+            imagenes.appendChild(logo);
+        }
         let masInfo = document.createElement('button');
         masInfo.className = "color4";
         masInfo.innerHTML= "Más información"
-        curso.append(nombre, imagenes, inicio, masInfo);
+        curso.append(nombre, imagenes, masInfo);
         contenido.appendChild(curso);
     }
 }
